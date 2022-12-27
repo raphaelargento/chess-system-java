9a93271839498c6390dfe81f58e4b66493f7feb7
Raphael Argento <raphaelargento@gmail.com>
2022-12-27 15:58:09 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,17 +1,39 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
     protected Position position;
     private Board board;
 
+    
     public Piece(Board board) {
         this.board = board;
         position = null;
     }
 
+
     // We don't allow the Board to be changed. That's why we have only the 'get' method
     // (protected) Just classes inside the same package or subclasses will access one piece's board
     protected Board getBoard() {
         return board;
+    }
+
+
+    public abstract boolean[][] possibleMoves();
+
+    public boolean possibleMove(Position position){
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+
+    public boolean isThereAnyPossibleMove(){
+        boolean[][] mat = possibleMoves();
+        for(int i=0; i<mat.length; i++){
+            for(int j=0; j<mat.length; j++){
+                if(mat[i][j]){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -10,8 +10,16 @@ public class King extends ChessPiece{
         super(board, color);
     }
     
+
     @Override
     public String toString(){
         return "K";
+    }
+
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        return mat;
     }
 }
