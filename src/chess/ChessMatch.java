d33ee6386aca64764a60fcd8c3e9ade07b60b3d2
Raphael Argento <raphaelargento@gmail.com>
2022-12-28 23:34:47 -0300

    Switching player each turn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -10,11 +10,25 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
     
+    private int turn;
+    private Color currentPlayer;
     private Board board;
 
     public ChessMatch(){
         board = new Board(8, 8);
+        turn = 1;
+        currentPlayer = Color.WHITE;
         initialSetup();
+    }
+
+
+    public int getTurn(){
+        return turn;
+    }
+    
+
+    public Color getCurrentPlayer(){
+        return currentPlayer;
     }
 
 
@@ -42,6 +56,7 @@ public class ChessMatch {
         validateSourcePosition(source);
         validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
+        nextTurn();
         return (ChessPiece)capturedPiece;
     }
 
@@ -49,6 +64,9 @@ public class ChessMatch {
     private void validateSourcePosition(Position position){
         if(!board.thereIsAPiece(position)){
             throw new ChessException("There is no piece on source position");
+        }
+        if(currentPlayer != (((ChessPiece) board.piece(position)).getColor())){
+            throw new ChessException("The choosen piece is not yours");
         }
         if(!board.piece(position).isThereAnyPossibleMove()){
             throw new ChessException("There is no possible moves for the choosen piece");
@@ -68,6 +86,12 @@ public class ChessMatch {
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
         return capturedPiece;
+    }
+
+
+    private void nextTurn(){
+        turn++;
+        currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
     }
 
 
